791ccc7607cf3fcd27fc2aab5cf17c050acf9e45
Phil Hartmann <philerichartmann@proton.me>
2024-12-02 21:22:23 +0100

    BIG CLEANUP

--- Semester1/Programmieren 1/Laborarbeit/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-if (obere_grenze < untere_grenze) // Für nicht besetzte Noten fragezeichen ausgeben
-            {
-                printf("Note %.1f: %d%% <= x < %d%% (?-? Punkte)\n", note, prozent, prozent + 5);
-                continue;
-            }
